1f8229e664111521612bcb8597af248efb5674d7
GabrielLopes03 <138938557+GabrielLopes03@users.noreply.github.com>
2023-08-22 22:19:45 -0300

    Add files via upload

---
+++ js/codigo1.js
@@ -0,0 +1,98 @@
+/*
+ * NODE.JS - É um S oftware de código aberto, multiplataforma
+ * que permite a execução * de JavaScript fora do navegador
+*/
+ 
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* criando uma matriz com elementos */
+
+/* console.log("Óla Mundo")
+
+let aluno = ["Zé", "pezinho", "sempre no CEL", "um deixou o cel"];
+let nota1 = 1;
+let nota2 = 3;
+let nota3 = 8.5;
+
+console.table(aluno);
+console.log(aluno[0]) */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* PUSH - Inclui um elemento no final da matriz */
+
+/* let aluno = ["Zé", "pezinho", "sempre no CEL", "um deixou o cel"];
+
+aluno.push("SENAI")
+console.table(aluno); */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* UNSHIFT - Inclui um elemento no começo da raiz */
+
+/* let aluno = [];
+
+aluno.push("Gabriel")
+aluno.unshift("Lopes")
+console.table(aluno); */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* POP -  Remove o ultimo elemento*/
+
+/* let aluno = ["Gabriel", "Zé", "Maria", "Pedro"];
+
+aluno.pop();
+console.table(aluno); */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* SHIFT - Remove o primeiro elemento */
+
+/* let aluno = ["Gabriel", "Zé", "Maria", "Pedro"];
+
+aluno.shift();
+console.table(aluno); */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* LENGTH - Mostra o tamanho da matriz */
+
+/* let aluno = ["Gabriel", "Zé", "Maria", "Pedro"];
+
+console.log("Tamanho da Matriz : " + aluno.length); */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* Podemos trocar um elemento dentro da matriz*/
+
+/* let aluno = ["Gabriel", "Zé", "Maria", "Pedro"];
+
+aluno[1] = "Maria do Carmo"
+console.table(aluno); */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* DELETE - deleta o elemento desejado */
+
+/* let aluno = ["Gabriel", "Zé", "Maria", "Pedro"];
+
+delete aluno(2);
+console.table(aluno);  */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+/* TABLE - cria uma tabela com os elementos */
+
+/* let cadastro = [
+    ["Gabriel", 9,10,5.5,8],
+    ["Josiane", 12,10,12,5],
+    ["Pedro", 10,8.5,9.5,10],
+    ["Ana", 5,5.5,6,7],
+    ["Claudia", 19,3.6,8.5,2.5]
+];
+
+console.table(cadastro)
+console.log(cadastro[2][1]);   */
+
+/* ----------------------------------------------------------------------------------------------------------------- */
